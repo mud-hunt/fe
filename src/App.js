ec6e3a21cc3a4915bd638a610e1c08643341e53e
Haja Andriamaro <haja.andri@gmail.com>
2019-10-22 12:25:47 +0200

    initial draft for first message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <AppContainer>
+      <MainDialog>
+        <div className="row">
+          <h1>Welcome to <br/>Mud-Hunt</h1>
+        </div>
+        <div className="row to-the-right">
+          <button>LETS PLAY</button>
+        </div>      
+      </MainDialog>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  background: #ff7577;
+  height:100vw;
+`;
+
+const MainDialog = styled.div`
+  width: 60%;
+  padding-top:20vw;
+  margin:0 auto;
+  h1 {
+    color:white;
+  }
+`;
