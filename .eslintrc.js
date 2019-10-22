dfa53ebaa026a3c6cf75fbcd5b69517ebf19f57f
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-22 19:46:35 +0100

    Chores

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings"
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  plugins: ["react"],
-  rules: {}
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es6: true
+//   },
+//   extends: [
+//     "prettier",
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:import/errors",
+//     "plugin:import/warnings"
+//   ],
+//   globals: {
+//     Atomics: "readonly",
+//     SharedArrayBuffer: "readonly"
+//   },
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true
+//     },
+//     ecmaVersion: 2018,
+//     sourceType: "module"
+//   },
+//   plugins: ["react"],
+//   rules: {}
+// };
