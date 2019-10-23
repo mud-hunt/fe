43dcb4a70505901f126c3c10a407b21d7300a9ba
Haja Andriamaro <haja.andri@gmail.com>
2019-10-23 21:00:55 +0200

    loading fake data from fetch for now

--- src/authHandlers/authHandlers.js
+++ src/authHandlers/authHandlers.js
@@ -60,17 +60,28 @@ export const loginHandler = ({ username, password }) => {
     });
 };
 
+// export const getRoomData = () => {
+//   axios.defaults.withCredentials = true
+//   axios.get('https://mud-hunt-be.herokuapp.com/api/adv/init/',
+//   setHeaders())
+//       .then( (response) => {
+//           // handle success
+//           console.log('succsess');
+//           return response;
+//       })
+//       .catch((error) => {
+//           // handle error
+//           console.log('The error is', error);
+//       })
+// }
+
 export const getRoomData = () => {
-  axios.defaults.withCredentials = true
-  axios.get('https://mud-hunt-be.herokuapp.com/api/adv/init/',
-  setHeaders())
-      .then( (response) => {
-          // handle success
-          console.log('succsess');
-          return response;
-      })
-      .catch((error) => {
-          // handle error
-          console.log('The error is', error);
-      })
+  return {
+    "uuid": "14544c34-6edc-4d94-b44d-ddf5538188fb",
+    "name": "testuser",
+    "roomId": 28,
+    "title": "Et et consequat eu elit in laboris dolor mollit.",
+    "description": "Quis incididunt consectetur nisi laborum mollit voluptate.",
+    "players": ['player1', 'player2', 'player3']
+  }
 }
