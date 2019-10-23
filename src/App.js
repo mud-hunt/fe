9fda254929b59bf4958b91c403fb3a9437387eb3
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-23 09:11:07 +0100

    chore: linting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import styled from 'styled-components';
-import Welcome from './components/Welcome';
-import Auth from './components/Auth'
-
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
+import Welcome from "./components/Welcome";
+import Auth from "./components/Auth";
 
 function App() {
-
   return (
     <Router>
       <Switch>
         <AppContainer>
-        <MainDialog>
-          <Route exact path="/" render={props => <Welcome {...props} />} />
-          <Route exact path="/auth" render={props => <Auth {...props} />} />
-        </MainDialog>
+          <MainDialog>
+            <Route exact path="/" render={props => <Welcome {...props} />} />
+            <Route exact path="/auth" render={props => <Auth {...props} />} />
+          </MainDialog>
         </AppContainer>
       </Switch>
     </Router>
@@ -25,23 +22,23 @@ function App() {
 export default App;
 
 const AppContainer = styled.div`
-  height:100%;
+  height: 100%;
 `;
 
 const MainDialog = styled.div`
-  display:flex;
-  flex-direction:column;
-  align-content:center;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
   width: 60%;
-  max-width:900px;  
-  min-width:400px;  
-  padding-top:10vw;
-  margin:0 auto;
+  max-width: 900px;
+  min-width: 400px;
+  padding-top: 10vw;
+  margin: 0 auto;
   h1 {
-    color:white;
+    color: white;
   }
   @media (max-width: 500px) {
-    padding-top:10vw;
-    min-width:100%;  
+    padding-top: 10vw;
+    min-width: 100%;
   }
 `;
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,87 +1,87 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
+const Auth = props => {
+  const [action, setAction] = useState("register");
 
-const Auth = (props) => {
+  const [details, updateInputChangeState] = useState({
+    username: "",
+    password1: "",
+    password2: ""
+  });
 
-    const [ action, setAction ] = useState('register')
-    
-    const [details, updateInputChangeState] = useState({
-        username: '',
-        password1: '',
-        password2: ''
+  const handleChange = event => {
+    event.preventDefault();
+    updateInputChangeState({
+      ...details,
+      [event.target.name]: event.target.value
     });
+  };
 
-    const handleChange = (event) => {
-        event.preventDefault();
-        updateInputChangeState({
-            ...details,
-            [event.target.name]: event.target.value,
-        });      
-    };
+  const toggleStyle = event => {
+    event.preventDefault();
+    document.getElementById("login").classList.toggle("blur");
+    document.getElementById("register").classList.toggle("blur");
+    setAction(event.target.id);
+  };
 
-    const toggleStyle = (event) =>{
-        event.preventDefault();
-        document.getElementById('login').classList.toggle('blur');
-        document.getElementById('register').classList.toggle('blur');
-        setAction(event.target.id);
-    }
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log("submitting");
+  };
 
-    const onSubmit = (event) =>{
-        event.preventDefault();
-        console.log("submitting");
-        
-    }
-
-        return(
-            <>
-            <div className="row center">
-                <h2>Before you play</h2>
-            </div>
-            <div className="row center">
-                    <h3 id="register" onClick={toggleStyle} className="title-choice">Register</h3>
-                    <h3>|</h3>
-                    <h3 id="login" onClick={toggleStyle} className="title-choice blur">Login</h3>
-                </div>
-                <form onSubmit={onSubmit}>
-                    <div className="row center">
-                    <input
-                        type="text"
-                        placeholder="Your user name"
-                        name="username"
-                        className="half"
-                        value={details.username}
-                        onChange={handleChange}
-                    />
-                    </div>
-                    <div className="row center">
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password1"
-                        className="half"
-                        value={details.password1}
-                        onChange={handleChange}
-                    />
-                    </div>
-                    <div className="row center">
-                    {
-                        action == 'register' &&
-                        <input
-                        type="password"
-                        placeholder="Confirm password"
-                        name="password2"
-                        className="half"
-                        value={details.password2}
-                        onChange={handleChange}
-                    />
-                    }
-                    </div>
-                    <div className="row center">
-                        <button className="big">SUBMIT</button>
-                    </div>
-                </form>
-            </>
-        )    
-}
+  return (
+    <>
+      <div className="row center">
+        <h2>Before you play</h2>
+      </div>
+      <div className="row center">
+        <h3 id="register" onClick={toggleStyle} className="title-choice">
+          Register
+        </h3>
+        <h3>|</h3>
+        <h3 id="login" onClick={toggleStyle} className="title-choice blur">
+          Login
+        </h3>
+      </div>
+      <form onSubmit={onSubmit}>
+        <div className="row center">
+          <input
+            type="text"
+            placeholder="Your user name"
+            name="username"
+            className="half"
+            value={details.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="row center">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password1"
+            className="half"
+            value={details.password1}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="row center">
+          {action == "register" && (
+            <input
+              type="password"
+              placeholder="Confirm password"
+              name="password2"
+              className="half"
+              value={details.password2}
+              onChange={handleChange}
+            />
+          )}
+        </div>
+        <div className="row center">
+          <button className="big">SUBMIT</button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default Auth;
