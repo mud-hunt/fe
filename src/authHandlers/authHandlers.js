5dcc4108c49b4dab2ccfbe7d597899ec67d931d9
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-23 14:45:53 +0100

    created login handler function

--- src/authHandlers/authHandlers.js
+++ src/authHandlers/authHandlers.js
@@ -8,11 +8,32 @@ export const registrationHandler = ({
 }) => {
   return axios
     .post("https://mud-hunt-be.herokuapp.com/api/registration/", {
-      username: username,
-      password1: password1,
-      password2: password2,
-      email: email
+      username,
+      password1,
+      password2,
+      email
     })
+    .then(response => {
+      console.log(response);
+      const token = response.data.key;
+      const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
+      localStorage.setItem("token", token);
+      localStorage.setItem("expirationDate", expirationDate);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export const loginHandler = ({ username, password }) => {
+  return axios
+    .post(
+      "https://mud-hunt-be.herokuapp.com/api/login/",
+      {
+        username,
+        password
+      },
+    )
     .then(response => {
       console.log(response);
       const token = response.data.key;
