73987c5c5b42248ca9bb1e21115097c8b53af385
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-24 12:36:02 +0100

    added error handling

--- src/authHandlers/authHandlers.js
+++ src/authHandlers/authHandlers.js
@@ -10,52 +10,56 @@ const setHeaders = () => {
   return {
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `${token}`,
-      "Referer": "https://mud-hunt-be.herokuapp.com"
+      Authorization: `${token}`,
+      Referer: "https://mud-hunt-be.herokuapp.com"
+      // "Access-Control-Allow-Origin": "https://mud-hunt-be.herokuapp.com"
     }
   };
 };
 
-export const registrationHandler = ({
-  username,
-  email,
-  password1,
-  password2
-}) => {
+export const registrationHandler = ({ username, password1, password2 }) => {
   return axios
-    .post("https://mud-hunt-be.herokuapp.com/api/registration/", {
-      username,
-      password1,
-      password2,
-      email
-    })
+    .post(
+      "https://mud-hunt-be.herokuapp.com/api/registration/",
+      {
+        username,
+        password1,
+        password2
+      },
+      setHeaders()
+    )
     .then(response => {
-      console.log(response);
-      const token = response.data.key;
-      const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
-      localStorage.setItem("token", token);
-      localStorage.setItem("expirationDate", expirationDate);
+      if (response && response.statusText === "OK") {
+        const token = response.data.key;
+        localStorage.setItem("token", token);
+        return response;
+      }
     })
     .catch(error => {
       console.log(error);
+      // throw error;
     });
 };
 
 export const loginHandler = ({ username, password }) => {
   return axios
-    .post("https://mud-hunt-be.herokuapp.com/api/login/", {
-      username,
-      password
-    },
-    setHeaders())
+    .post(
+      "https://mud-hunt-be.herokuapp.com/api/login/",
+      {
+        username,
+        password
+      },
+      setHeaders()
+    )
     .then(response => {
-      console.log(response);
-      const token = response.data.key;
-      const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
-      localStorage.setItem("token", token);
-      localStorage.setItem("expirationDate", expirationDate);
+      if (response.status === 200) {
+        const token = response.data.key;
+        localStorage.setItem("token", token);
+        return response;
+      }
     })
     .catch(error => {
       console.log(error);
+      throw error;
     });
 };
