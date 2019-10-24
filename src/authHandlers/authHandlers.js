6f5282d44676f1555294a19322dcaaa69dbbab77
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-24 14:49:18 +0100

    removed commented code

--- src/authHandlers/authHandlers.js
+++ src/authHandlers/authHandlers.js
@@ -29,20 +29,9 @@ export const registrationHandler = ({ username, password1, password2 }) => {
     .then(response => {
       const token = response.data.key;
       localStorage.setItem("token", token);
-      console.log("hellothen")
       return response;
     })
     .catch(error => {
-      /* console.log("hellocatch")
-      let errorString = "";
-      error.forEach(element => {
-        if (Array.isArray(element)) {
-          for (let i = 0; i < element.length; i++) {
-            errorString += ", ".concat(element[i]);
-          }
-        }
-      });
-      throw new Error(errorString); */
       throw error;
     });
 };
