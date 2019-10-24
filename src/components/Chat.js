a0a28733bd8bc87929e001db728ca4455dcc138d
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-24 19:39:01 +0100

    Subscribe to message channel when pusher instance created

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Chat = (props) => {
+  const {pusher} = props;
+  let messageChannel;
 
+  useEffect(() => {
+    if (pusher) {
+      messageChannel = pusher.subscribe('message-channel');
+    }
+  }, [pusher])
 }
