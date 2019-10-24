15dc4c943d973105282166f7a64603849783b53d
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-24 21:25:23 +0100

    Add update component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
-import styled from 'styled-components';
-import { sendMessage } from '../authHandlers/authHandlers';
+import React, { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
+import { sendMessage } from "../authHandlers/authHandlers";
 
-const Chat = (props) => {
+const Chat = props => {
   const { pusher } = props;
   const [messageChannel, setMessageChannel] = useState();
   const [messages, setMessages] = useState([]);
@@ -10,27 +10,28 @@ const Chat = (props) => {
   const chatBox = useRef();
 
   const onMessage = message => {
+    message.type = "message";
     setMessages(state => state.concat([message]));
     chatBox.current.scrollTop = chatBox.current.scrollHeight;
-  }
+  };
 
   useEffect(() => {
     if (pusher) {
-      setMessageChannel(pusher.subscribe('message-channel'));
+      setMessageChannel(pusher.subscribe("message-channel"));
     }
   }, [pusher]);
 
   useEffect(() => {
     if (messageChannel) {
-      messageChannel.bind('message', onMessage);
+      messageChannel.bind("message", onMessage);
     }
   }, [messageChannel]);
-
 
   const onSend = e => {
     e.preventDefault();
     sendMessage(messageBox.current.value);
-  }
+    messageBox.current.value = '';
+  };
 
   return (
     <>
@@ -39,49 +40,53 @@ const Chat = (props) => {
       </ChatTitle>
       <ChatContent ref={chatBox}>
         {messages.map(message => {
-          return (
-            <Message>
-              <span>{message.username}: </span>
-              {message.message}
-            </Message>
-          );
+          if (message.type === 'message') {
+            return (
+              <Message>
+                <span>{message.username}: </span>
+                {message.message}
+              </Message>
+            );
+          }
+          if (message.type === 'update') {
+            return <Update>message.message</Update>;
+          }
+          return null;
         })}
       </ChatContent>
       <MessageBox onSubmit={onSend}>
-        <input
-          type='text'
-          placeholder='Message...'
-          ref={messageBox}
-        />
-        <input
-          type='submit'
-          value='Send'
-        />
+        <input type="text" placeholder="Message..." ref={messageBox} />
+        <input type="submit" value="Send" />
       </MessageBox>
     </>
   );
-}
+};
 
 export default Chat;
 
 const ChatTitle = styled.div`
-    background:white
-    h3{
-        color:#ff7577;
-    }
+  background:white h3 {
+    color: #ff7577;
+  }
 `;
 
 const ChatContent = styled.div`
-    color:white;
-    height: 20rem;
-    overflow-y: scroll;
-    padding-left:5px;
+  color: white;
+  height: 20rem;
+  overflow-y: scroll;
+  padding-left: 5px;
 `;
 
-const Message = styled.h4`
+const Message = styled.p`
   > span {
-    font-weight: 400;
+    font-weight: 600;
   }
+  font-weight: 200;
+`;
+
+const Update = styled.p`
+  font-style: italic;
+  font-weight: 200;
 `;
 
 const MessageBox = styled.form`
