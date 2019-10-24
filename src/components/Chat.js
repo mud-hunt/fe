8c797b8d97feca07b5c41e80fedc59de44b24387
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-24 19:46:56 +0100

    Add some basic styles

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,12 +2,32 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Chat = (props) => {
-  const {pusher} = props;
-  let messageChannel;
+  const { pusher } = props;
+  const [messageChannel, setMessageChannel] = useState();
 
   useEffect(() => {
     if (pusher) {
-      messageChannel = pusher.subscribe('message-channel');
+      setMessageChannel(pusher.subscribe('message-channel'));
     }
-  }, [pusher])
+  }, [pusher]);
 }
+
+const ChatTitle = styled.div`
+    background:white
+    h3{
+        color:#ff7577;
+    }
+`;
+
+const ChatContent = styled.div`
+    color:white;
+    height: 20rem;
+    overflow-y: scroll;
+    padding-left:5px;
+`;
+
+const Message = styled.h4`
+  > span {
+    font-weight: bold;
+  }
+`;
